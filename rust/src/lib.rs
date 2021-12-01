8cd5160b35fd4d0a7cfd74dbe32fbca2d972bc74
Björn Westlin <bwestlin@gmail.com>
2021-12-01 01:29:39 +0100

    Prepare scripts and such for rust

---
+++ rust/src/lib.rs
@@ -0,0 +1,43 @@
+extern crate time;
+
+use time::*;
+
+pub fn measure<F, S, T>(f: F) -> Result<S, T>
+where
+    F: Fn() -> Result<S, T>,
+{
+    let start = precise_time_ns();
+    let mut _times = 100;
+
+    let res = f();
+
+    #[cfg(feature = "timeit")]
+    {
+        let mut res = res;
+        let dur_ns = precise_time_ns() - start;
+        if dur_ns > 500_000_000 {
+            _times /= 10;
+        } else if dur_ns < 500_000 {
+            _times *= 10;
+        }
+        for _ in 0..(_times - 1) {
+            res = f();
+        }
+    }
+
+    let dur_ns = precise_time_ns() - start;
+
+    #[cfg(feature = "timeit")]
+    {
+        println!(
+            "It took: {}ms on average for {} times",
+            (dur_ns / _times as u64) as f64 / 1_000_000.0,
+            _times
+        );
+    }
+    #[cfg(not(feature = "timeit"))]
+    {
+        println!("It took: {}ms", dur_ns as f64 / 1_000_000.0);
+    }
+    res
+}
